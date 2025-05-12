5c6d76d88157a7452be196f55182cfef3beafae7
arthursouto09 <151585884+arthursouto09@users.noreply.github.com>
2025-05-12 17:13:43 -0300

    Adicionando a classe aluno

---
+++ sistemaAcademico/ep1/src/Aluno.java
@@ -0,0 +1,60 @@
+public class Aluno {
+    private String nome;
+    private int matricula; 
+    private String curso; 
+
+
+
+    public Aluno (String nome, int matricula, String curso) {
+    this.nome = "";
+    this.matricula = 0;
+    this.curso = curso;
+    }
+
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void getnome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void getMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void getCurso(String curso) {
+        this.curso = curso;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+    
