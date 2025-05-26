4f82bd764a325bb24d79bd587ea8ef3bf9801944
arthursouto09 <151585884+arthursouto09@users.noreply.github.com>
2025-05-26 18:46:28 -0300

    Adiconando a classe professor e pessoa. Que faz herancça com a classe pessoa

---
+++ sistemaAcademico/ep1/src/pessoas/Pessoa.java
@@ -0,0 +1,27 @@
+package pessoas;
+
+public abstract class Pessoa {
+    protected String nome;
+    protected int id;
+
+    public Pessoa(String nome, int id) {
+        this.nome = nome;
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+}
---
+++ sistemaAcademico/ep1/src/pessoas/Professor.java
@@ -0,0 +1,29 @@
+package pessoas;
+
+public class Professor extends Pessoa {
+    private String disciplina;
+
+    public Professor(String nome, int id, String disciplina) {
+        super(nome, id);
+        this.disciplina = disciplina;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
+
+    public void setDisciplina(String disciplina) {
+        this.disciplina = disciplina;
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "nome='" + nome + '\'' +
+                ", id=" + id +
+                ", disciplina='" + disciplina + '\'' +
+                '}';
+    }
+
+
+}
